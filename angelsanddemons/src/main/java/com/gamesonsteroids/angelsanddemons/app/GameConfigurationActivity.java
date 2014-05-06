b9deee16c863543297c941dce5f2b750cf9f1e01
Kikaimaru <kikaimaru@windowslive.com>
2014-05-06 13:31:11 +0200

    incorrect player names handling

--- angelsanddemons/src/main/java/com/gamesonsteroids/angelsanddemons/app/GameConfigurationActivity.java
+++ angelsanddemons/src/main/java/com/gamesonsteroids/angelsanddemons/app/GameConfigurationActivity.java
@@ -3,9 +3,7 @@ package com.gamesonsteroids.angelsanddemons.app;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,15 +17,31 @@ public class GameConfigurationActivity extends ListActivity {
     public void onStartGameClick(View v) {
         // validate player names
 
+        extractPlayerNames();
+
         Intent intent = new Intent(GameConfigurationActivity.this, AssignRolesActivity.class);
         GameConfigurationActivity.this.startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        extractPlayerNames();
+
+        super.onSaveInstanceState(outState);
+    }
+
+    private void extractPlayerNames() {
+        for (int i = 0; i < getListView().getChildCount(); i++) {
+            CharSequence playerName = ((EditText)getListView().getChildAt(i).findViewById(R.id.playerName)).getText();
+            GameSession.getCurrent().getPlayers().get(i).setName(playerName);
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_configuration);
-
 
 
         setListAdapter(new ListAdapter<Player>(GameSession.getCurrent().getPlayers(), new ListAdapter.ListViewProvider<Player>() {
@@ -43,18 +57,6 @@ public class GameConfigurationActivity extends ListActivity {
                 EditText playerName = (EditText)view.findViewById(R.id.playerName);
 
                 playerName.setText(item.getName());
-                playerName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                    @Override
-                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                        if (actionId == EditorInfo.IME_ACTION_DONE) {
-
-                            item.setName(v.getText());
-                            return  true;
-                        }
-
-                        return false;
-                    }
-                });
 
                 return view;
             }
