361a73e96b454e0aa2a71c7ea3559f1b1481f4cb
Kikaimaru <kikaimaru@windowslive.com>
2014-11-16 20:08:04 +0100

    stuff

---
+++ angelsanddemons/src/main/java/com/gamesonsteroids/angelsanddemons/app/GameOverActivity.java
@@ -0,0 +1,21 @@
+package com.gamesonsteroids.angelsanddemons.app;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.gamesonsteroids.angelsanddemons.game.GameRules;
+import com.gamesonsteroids.angelsanddemons.game.GameSession;
+import com.gamesonsteroids.angelsanddemons.game.Player;
+import com.gamesonsteroids.angelsanddemons.widgets.ListAdapter;
+
+
+public class GameOverActivity extends AbstractGameActivity {
+
+
+
+}
